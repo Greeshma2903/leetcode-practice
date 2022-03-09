0f78f95c86a43f3e0e845b27b79aaffecd6bfdc4
M Greeshma <70336930+Greeshma2903@users.noreply.github.com>
2022-03-09 12:58:44 +0530

    Time: 7 ms (87.75%), Space: 7.4 MB (46.97%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -7,20 +7,41 @@ public:
         if(s.length() != t.length())
             return false;
         
+//         APPROACH 2
         for(i = 0; i < s.length(); i++)
         {
+            // first string: increment positional values
             first[s[i] - 97]++;
+            
+            // second string: decrement postional values
+            first[t[i] - 97]--;
         }
-        for(i = 0; i < t.length(); i++)
+            
+        // if value at any postion is negative, means that the character is not present in the other string/is less than required value. hence, return 'false'.
+        for(i =0; i < 26; i++)
         {
-            second[t[i] - 97]++;
-        }
-
-        for(i = 0; i < 26; i++)
-        {
-            if(first[i] != second[i])
+            if(first[i] < 0)
                 return false;
         }
+        
         return true;
     }
 };
+
+//      APPROACH 1: 
+
+//         for(i = 0; i < s.length(); i++)
+//         {
+//             first[s[i] - 97]++;
+//         }
+//         for(i = 0; i < t.length(); i++)
+//         {
+//             second[t[i] - 97]++;
+//         }
+
+//         for(i = 0; i < 26; i++)
+//         {
+//             if(first[i] != second[i])
+//                 return false;
+//         }
+//         return true;
