2f6d6dcfa9f317a78be543d1dfc56174f65a2868
Medam Greeshma <70336930+Greeshma2903@users.noreply.github.com>
2024-02-10 09:08:23 +0530

    Time: 0 ms (100%), Space: 7.7 MB (16.66%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.cpp
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -1,32 +1,22 @@
 class Solution {
 public:
+    string compareStrings(string str) {
+        string newStr = ""; // to store new strings without backspace
+        int count = 0; // to keep count of backspace in strings
+        for (int i = str.size() - 1; i >= 0; i--) {
+            if (str[i] == '#')
+                count++;
+            else if (str[i] != '#' && count > 0) {
+                count--;
+            } else {
+                newStr.push_back(str[i]);
+            }
+        }
+        return newStr;
+    }
     bool backspaceCompare(string s, string t) {
-        // Approach 1: O(n) space complexity
-        stack<char> str1, str2;
-        for (int i = 0; i < s.size(); i++) {
-            if (s[i] == '#') {
-                // if #, pop element if stack isn't empty; continues to remain
-                // empty or else
-                if (!str1.empty())
-                    str1.pop();
-            } else {
-                // push other elements
-                str1.push(s[i]);
-            }
-        }
-        for (int i = 0; i < t.size(); i++) {
-            if (t[i] == '#') {
-                // if #, pop element if stack isn't empty; continues to remain
-                // empty or else
-                if (!str2.empty())
-                    str2.pop();
-            } else {
-                // push other elements
-                str2.push(t[i]);
-            }
-        }
-
-        // compare is contain same value
-        return str1 == str2;
+        string str1 = compareStrings(s);
+        string str2 = compareStrings(t);
+        return (str1 == str2);
     }
 };
