7cbab7c1bbaa4d9c2312792561ae14079026b7ad
Medam Greeshma <70336930+Greeshma2903@users.noreply.github.com>
2024-03-03 19:02:19 +0530

    Time: 7 ms (73.97%), Space: 21 MB (46.16%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -3,21 +3,20 @@ public:
     int removeDuplicates(vector<int>& nums) {
         // Method 1: Using external array and storing unique values
         // Method 2: Using two pointer approach
-        if(nums.size() == 1) return 1;
-        
+        if (nums.size() == 1)
+            return 1;
+
         int ptr1 = 1;
         int ptr2 = 1;
         // iterate through array
-        while(ptr2 < nums.size()){
+        while (ptr2 < nums.size()) {
             // initial pointer keeps track of unique elements
             // while the end pointer helps compare next unique value in array
-            if (nums[ptr1-1] == nums[ptr2]) {
-                ptr2++;
-            } else {
+            if (nums[ptr1 - 1] != nums[ptr2]) {
                 nums[ptr1] = nums[ptr2];
                 ptr1++;
-                ptr2++;
             }
+            ptr2++;
         }
         return ptr1;
     }
